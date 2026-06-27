fdafbca0f0a0be1b9cca6b53c1da4b33605debfa
laudavidtw-netizen <lau.davidtw@gmail.com>
2026-06-27 22:20:33 +0800

    Update API key reference in chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -23,7 +23,7 @@ if "messages" not in st.session_state.keys(): # Initialize the chat message hist
 # llm = ChatOpenAI(model_name="gpt-4o-mini")
 # llm = ChatGoogleGenerativeAI(model = "gemini-pro")
 llm = ChatOpenAI(model = "gpt-4o-mini",
-                      openai_api_key = st.secrets["TOGETHER_API_KEY"] , ## use your key
+                      openai_api_key = st.secrets["OPENAI_API_KEY"] , ## use your key
                       openai_api_base = "https://api.together.xyz/v1"
 
 )
